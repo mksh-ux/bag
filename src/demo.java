0ca4452ed5a47ceb6dbfc184b33ac87c341c9300
mksh-ux <anukrishnan237@gmail.com>
2026-09-02 13:21:41 +0530

    first commit

---
+++ src/demo.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.time.Clock;
+import java.util.Scanner;
+
+public class demo {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
+
+
+
